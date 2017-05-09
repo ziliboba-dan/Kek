c730c3ab32c37490022f43ec7341a274dabfb239
danil <EvtushenkoAleksei@yandex.ru>
2017-05-09 13:23:24 +0700

    Version 1.0

--- PALINDROM/src/palindrom.c
+++ PALINDROM/src/palindrom.c
@@ -4,6 +4,70 @@
 #include <string.h>
 #include "palindrom.h"
 
+int palindrom_func(char *str, int size)
+{
+	int k = 0;
+	char *str1 = malloc(sizeof(char) * size);
+	if (str1 == NULL) {
+		return -1;
+	}
+	for (int i = 0; i < strlen(str); i++) {
+		if ((str[i] != '.' && str[i] != '!' && str[i] != '?')) {
+			if ((str[i] != ' ')) {
+				str1[k] = str[i];
+				k++;
+			}
+		}
+		else {	
+			//printf("%s ", str1);
+			//printf("%d\n", strlen(str1));
+			int znach = 1;
+			for (int j = 0; j < strlen(str1); j++) {
+				//printf("%d  ", str1[j]);
+				//printf("%d\n", str1[strlen(str1) - 1 - j]);
+				if (((str1[j]) != (str1[strlen(str1) - 1 - j])) && ((str1[j]) != (str1[strlen(str1) - 1 - j] - 32)) && ((str1[j] - 32) != (str1[strlen(str1) - 1 - j]))) {
+					znach = 0;
+				}
+			}
+			if (znach) {
+				printf("%s", str1);
+				printf("\n");
+			}
+			memset(str1, 0 ,strlen(str1));
+			k = 0;
+		}
+	}
+	free(str1);
+	return 0;
+}
+
+int delete_punct(char *buffer, int size)
+{
+	int ix = 0;
+	int is = 0;
+	char *str = malloc(sizeof(char) * size);
+	if (str == NULL) {
+		return -1;
+	}
+
+	while (ix < size) {
+		if((buffer[ix] != '.' && buffer[ix] != '!' && buffer[ix] != '?')){
+			//printf("%c", buffer[ix]);
+			if(!(ispunct(buffer[ix]) || (isdigit(buffer[ix]) || (buffer[ix] == '\n')))){
+				str[is] = buffer[ix];
+				is++;
+			}			
+		}
+		else {
+			str[is] = buffer[ix];
+			is++;
+		}
+		ix++;
+	}
+	strcpy(buffer, str);
+	return 0;
+}
+
 int process(const char *file_name)
 {
 	FILE *a;
@@ -33,57 +97,16 @@ int process(const char *file_name)
 	if (result != size ) {
 		return -1;
 	}
-	char *str = malloc(sizeof(char) * size);
-	if (str == NULL) {
+	//удаляем все знаки препинания и цифры
+	if (delete_punct(buffer, size) == -1) {
 		return -1;
 	}
-	printf("%d\n", strlen(buffer));
-	int ix = 0;
-	int is = 0;
-	//удаление всех знаков препинания и запись в второй буфер 
-	while (ix < size) {
-		if(!(ispunct(buffer[ix]) || (isdigit(buffer[ix]) || (buffer[ix] == '\n')))){
-			printf("%c", buffer[ix]);
-			str[is] = buffer[ix];
-			is++;
-		}
-		ix++;
-	}
-	printf("\n");
-	printf("%s\n", str);
-	//palindrom
-	int k = 0;
-	char *str1 = malloc(sizeof(char) * size);
-	if (str1 == NULL) {
+	printf("%s\n", buffer);
+	printf("Palindroms: \n");
+	//поиск палиндромов
+	if (palindrom_func(buffer, strlen(buffer)) == -1) {
 		return -1;
 	}
-	for (int i = 0; i < strlen(str); i++) {
-		if (str[i] != ' ') {
-			str1[k] = str[i];
-			k++;
-		}
-		else {
-			printf("%s ", str1);
-			printf("%d\n", strlen(str1));
-			int znach = 1;
-			for (int j = 0; j < strlen(str1); j++) {
-				printf("%c  ", str1[j]);
-				printf("%c\n", str1[strlen(str1) - 1 - j]);
-				if (tolower(str1[j]) != tolower(str1[strlen(str1) - 1 - j])) {
-					znach = 0;
-				}
-			}
-			if (znach) {
-				printf("%s", str1);
-				printf("\n");
-			}
-			memset(str1, 0 ,strlen(str1));
-			k = 0;
-		}
-	}
-	printf("%c\n", str1[0]);
-	free(str);
-	free(str1);
 	free(buffer);
 	fclose(a);
 	fclose(b);
